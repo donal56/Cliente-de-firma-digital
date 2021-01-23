0be45be7f7367579492902f0553b9a30aed91c6d
Carlos Donaldo Ramon Gomez <donal_56@hotmail.com>
2021-01-23 16:53:02 -0600

    Debug interface

---
+++ src/mx/com/insigniait/cliente_firma_sat/util/Debug.java
@@ -0,0 +1,31 @@
+package mx.com.insigniait.cliente_firma_sat.util;
+
+public class Debug {
+
+	/*
+	 * Mensaje de error fatal
+	 */
+	public static void fatal(String debugMsg) {
+		if(debugMsg != null && AppProperties.debug()) {
+			throw new RuntimeException("[FATAL] " + debugMsg);
+		}
+	}
+
+	/*
+	 * Mensaje de error
+	 */
+	public static void error(String debugMsg) {
+		if(debugMsg != null && AppProperties.debug()) {
+			System.err.println("[ERROR] " + debugMsg);
+		}
+	}
+	
+	/*
+	 * Mensaje de información
+	 */
+	public static void info(String debugMsg) {
+		if(debugMsg != null && AppProperties.debug()) {
+			System.out.println("[INFO] " + debugMsg);
+		}
+	}
+}

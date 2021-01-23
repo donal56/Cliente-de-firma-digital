8a2d3f777626d025e18a442176f3305af396c7fc
Carlos Donaldo Ramon Gomez <donal_56@hotmail.com>
2021-01-23 16:56:52 -0600

    Ordenando Main

--- src/mx/com/insigniait/cliente_firma_sat/gui/Main.java
+++ src/mx/com/insigniait/cliente_firma_sat/gui/Main.java
@@ -1,57 +1,33 @@
 package mx.com.insigniait.cliente_firma_sat.gui;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URL;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-import java.util.Arrays;
-import java.util.Base64;
 import java.util.Enumeration;
 import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicBoolean;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpHeaders;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.client.methods.RequestBuilder;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.mime.HttpMultipartMode;
-import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.springframework.util.StringUtils;
 
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.ChoiceDialog;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
-import mx.com.insigniait.cliente_firma_sat.dto.SatSigner;
-import mx.com.insigniait.cliente_firma_sat.services.SigningService;
-import mx.com.insigniait.cliente_firma_sat.services.TimestampAuthorityClient;
+import mx.com.insigniait.cliente_firma_sat.tasks.SignerTask;
+import mx.com.insigniait.cliente_firma_sat.util.AppProperties;
 import mx.com.insigniait.cliente_firma_sat.util.CredentialStorage;
-import mx.com.insigniait.cliente_firma_sat.util.Util;
+import mx.com.insigniait.cliente_firma_sat.util.Debug;
+import mx.com.insigniait.cliente_firma_sat.util.GuiUtils;
+import mx.com.insigniait.cliente_firma_sat.util.Utils;
 
 /*
  * https://jvmfy.com/2018/11/17/how-to-digitally-sign-pdf-files/
@@ -66,15 +42,15 @@ public class Main extends Application {
 	 * Argumentos de la aplicación
 	 */
 	private static Map<String, String> argumentos = null;
-	
+
 	/*
 	 * Punto de entrada de la aplicación
 	 */
 	public static void main(String[] args) throws IllegalStateException, FileNotFoundException, IOException {
-		
-		//Limpiando parametros de la aplicación
-		if(args.length > 0) {
-			argumentos = Util.getProtocolQueryParams(args[0]);
+
+		// Limpiando parametros de la aplicación
+		if (args.length > 0) {
+			argumentos 	= 	Utils.getProtocolQueryParams(args[0]);
 		}
 
 		Application.launch(args);
@@ -87,47 +63,54 @@ public class Main extends Application {
 	public void start(Stage stage) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
 		
 		//Comprobando parametros
-		if(argumentos == null || argumentos.size() == 0 || !argumentos.containsKey("req") || !argumentos.containsKey("ss")) {
-			throwError("Parámetros faltantes.");
-			return;
+		if(	argumentos == null || 
+			argumentos.size() == 0 || 
+			!argumentos.containsKey("req") || 
+			!argumentos.containsKey("ss")) {
+			
+			GuiUtils.throwError("Parámetros faltantes.", "Solicitud o secret-salt faltantes: " + argumentos.toString());
 		}
-	
-		String 				request 	= 	Util.decrypt(argumentos.get("req"), Util.getApplicationProperty("secret"), argumentos.get("ss"));
-		Map<String, String> parametros 	= 	Util.createMapFromCommaSeparatedString(request);
-		
-		if(StringUtils.isEmpty(parametros.get("doc")) || StringUtils.isEmpty(parametros.get("username")) 
-				|| StringUtils.isEmpty(parametros.get("password")) || StringUtils.isEmpty(parametros.get("referer")) 
-						|| StringUtils.isEmpty(parametros.get("docId"))) {
-			throwError("Solicitud no válida.");
-			return;
+
+		//Desencriptando parametros
+		String 				request 	= 	Utils.decrypt(argumentos.get("req"), AppProperties.get("secret"), argumentos.get("ss"));
+		Map<String, String> parametros 	= 	Utils.createMapFromCommaSeparatedString(request);
+
+		if(	StringUtils.isEmpty(parametros.get("doc")) || 
+			StringUtils.isEmpty(parametros.get("username")) || 
+			StringUtils.isEmpty(parametros.get("password")) || 
+			StringUtils.isEmpty(parametros.get("referer")) || 
+			StringUtils.isEmpty(parametros.get("docId"))) {
+			
+			GuiUtils.throwError("Solicitud no válida.", "URL del documento, ID del documento o datos de conexión(URL, usuario, contraseña) "
+					+ "faltantes: " + parametros.toString());
 		}
-		
+
 		// Etiquetas
 		Label labelCrt = new Label("Seleccionar certificado");
 		labelCrt.setPrefWidth(380);
-		
+
 		// Combo de certificados instalados
 		ComboBox<String> certCombo = new ComboBox<String>();
 		certCombo.setPrefWidth(380);
 		certCombo.setPromptText("--Seleccione uno--");
-		
+
 		Enumeration<String> aliases = CredentialStorage.getAliases();
-		
+
 		while (aliases.hasMoreElements()) {
 			certCombo.getItems().add(aliases.nextElement());
 		}
-		
+
 		// Icono de botón de formulario
 		ImageView checkIcon = new ImageView(new Image("check.png"));
 		checkIcon.setFitHeight(22);
 		checkIcon.setFitWidth(22);
-		
+
 		// Botón de formulario
 		Button btnElegir = new Button("Elegir");
 		btnElegir.setGraphic(checkIcon);
 		btnElegir.setPrefWidth(160);
-		Util.setEnterAsClick(btnElegir);
-		
+		GuiUtils.setEnterAsClick(btnElegir);
+
 		// Panel
 		FlowPane flowPane = new FlowPane();
 		flowPane.getChildren().add(labelCrt);
